fd7fbb5c57e02ccb4abae766f086b8f2e578b110
Ethan Kletschke <Bbruhingson@gmail.com>
2025-03-05 10:42:04 +0200

    Start adding comments.

--- Main.py
+++ Main.py
@@ -1,24 +1,33 @@
-# A program that generates random excel data in a .csv file
+# A program that generates random Excel data in a .csv file
+
+# Imports
 from os import _exit as stop
 from Errors import error_stop as errstop
 from Details import Details as dtls
 
-# Variables
+# The stopping row for the data.
 stopping_row = 0
+# The array holding the data.
 data = []
 
 try:
+    # Get user input for the row to stop generating data at.
     stopping_row = int(input("Stopping Row: "))
 except KeyboardInterrupt:
-    print("Exiting...")
+    # Exit the program on ctrl+C
+    print("\nExiting...")
     stop(0)
 except ValueError:
+    # Use the user-defined "errstop" method to exit the program.
     errstop("You didn't enter a number.")
 except Exception:
+    # Use the user-defined "errstop" method to exit the program.
     errstop("Unspecified Error.")
 else:
+    # Display that the program is writing to the file.
     print("Writing to file...")
 
+# Create the Details (with the alias "dtls") object
 details = dtls()
 for i in range(stopping_row + 1):
     details.randomise()
@@ -39,8 +48,11 @@ with open("Data.csv", "w+") as csv:
     # Print the headings
     csv.write("ID, first_name,last_name,age,city,amount_owed\n")
 
-    # From 1 to the index provided
+    # From 0 to the index provided
     for i in range(0, stopping_row):
+
         for j in range(0, 6):
+            # Write the data generated into the CSV file.
             csv.write(f"{data[i][j]},")
+        # Skip to the next line.
         csv.write("\n")
